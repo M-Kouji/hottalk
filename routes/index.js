1ff83811d0a8356fdcd176f066a4c94a46ea24cf
Koji Mineo <koji_mineo@icloud.com>
2021-04-03 22:45:29 +0900

    topページの構成作成

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,25 @@ const { Op } = require('sequelize');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('top.pug', { title: 'Hottalk TOPページ' });
 });
 
-router.get('/test', function(req, res, next) {
-  console.log('ログテスト');
-  // User.upsert({
-  //   username: 'test4',
-  //   password: 'test4'
-  // });
-  User.findAll({
-    where: {
-      userId: {
-        [Op.between]: [0,100]
-      }
-    }
-  }).then((Users)=>{
-    console.log(Users);
-  });
-  res.render('index', { title: 'Express' });
-});
+// router.get('/test', function(req, res, next) {
+//   console.log('ログテスト');
+//   User.upsert({
+//     username: 'test4',
+//     password: 'test4'
+//   });
+//   User.findAll({
+//     where: {
+//       userId: {
+//         [Op.between]: [0,100]
+//       }
+//     }
+//   }).then((Users)=>{
+//     console.log(Users);
+//   });
+//   res.render('top.pug', { title: 'Hottalk TOPページ' });
+// });
 
 module.exports = router;
