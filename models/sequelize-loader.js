54c91db1aa949bb4094a0a8afbcf13deefc0a92d
Koji Mineo <koji_mineo@icloud.com>
2021-04-07 15:51:54 +0900

    typo修正

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,9 +1,6 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/talk_arranger'
-);
-const sequelize = new Sequelize(
   process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/talk_arranger',
   {
     logging: false,
