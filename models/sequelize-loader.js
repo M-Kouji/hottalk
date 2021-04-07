d2e4f447ce0aeab18ade0d1f1f8c558de40ebfda
Koji Mineo <koji_mineo@icloud.com>
2021-04-07 14:52:15 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -3,6 +3,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
   'postgres://postgres:postgres@localhost/talk_arranger'
 );
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/talk_arranger'
+ );
 
 module.exports = {
   database: sequelize,
